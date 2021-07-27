71d9a7440d21862115cb169000568c527ae424fb
João Vítor <joaovsoutosantos@gmail.com>
2021-07-27 20:05:09 -0300

    Retrieve raw data from the four fields

--- iot_dashboard/static/js/index.js
+++ iot_dashboard/static/js/index.js
@@ -1,83 +1,138 @@
-const config = {
-  type: 'line',
-  data: {
-    labels: [],
-    datasets: [
-      {
-        label: 'Temperatura',
-        data: [],
-        borderWidth: 6,
-        borderColor: 'rgba(77,166,253,0.85)',
-        backgroundColor: 'transparent',
+function createChartConfig({ datasetLabel, xAxisLabel, yAxisLabel }) {
+  return {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: datasetLabel,
+          data: [],
+          borderWidth: 6,
+          borderColor: 'rgba(77,166,253,0.85)',
+          backgroundColor: 'transparent',
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: false,
       },
-    ],
-  },
-  options: {
-    responsive: true,
-    title: {
-      display: false,
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false,
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: true,
-    },
-    scales: {
-      xAxes: [
-        {
-          display: true,
-          scaleLabel: {
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true,
+      },
+      scales: {
+        xAxes: [
+          {
             display: true,
-            labelString: 'Tempo',
+            scaleLabel: {
+              display: true,
+              labelString: xAxisLabel,
+            },
           },
-        },
-      ],
-      yAxes: [
-        {
-          display: true,
-          scaleLabel: {
+        ],
+        yAxes: [
+          {
             display: true,
-            labelString: 'Temperatura ºC',
+            scaleLabel: {
+              display: true,
+              labelString: yAxisLabel,
+            },
           },
-        },
-      ],
+        ],
+      },
     },
-  },
-};
+  }
+}
 
 (() => {
-  const ctx = document.getElementById('canvas').getContext('2d');
-  window.myLine = new Chart(ctx, config);
+  const ctx = document.getElementById('raw-field-1').getContext('2d');
+  window.rawField1 = new Chart(ctx, createChartConfig({
+    datasetLabel: 'Outdoor Temperature',
+    xAxisLabel: 'Time',
+    yAxisLabel: 'Temperatura [ºC]',
+  }));
 })();
 
-async function getLastThingSpeakData() {
-  const CHANNEL_ID = 1293177;
+(() => {
+  const ctx = document.getElementById('raw-field-2').getContext('2d');
+  window.rawField2 = new Chart(ctx, createChartConfig({
+    datasetLabel: 'Temperature',
+    xAxisLabel: 'Time',
+    yAxisLabel: 'Temperatura [ºC]',
+  }));
+})();
+
+(() => {
+  const ctx = document.getElementById('raw-field-3').getContext('2d');
+  window.rawField3 = new Chart(ctx, createChartConfig({
+    datasetLabel: 'Air Pressure (BPM280)',
+    xAxisLabel: 'Time',
+    yAxisLabel: 'Pressure [mmHg]',
+  }));
+})();
+
+(() => {
+  const ctx = document.getElementById('raw-field-4').getContext('2d');
+  window.rawField4 = new Chart(ctx, createChartConfig({
+    datasetLabel: 'Humidity',
+    xAxisLabel: 'Time',
+    yAxisLabel: 'Humidity [%]',
+  }));
+})();
+
+async function getLastThingSpeakData({ chartInstance, fieldNumber }) {
+  const CHANNEL_ID = 196384;
 
   const response = await fetch(
-    `https://api.thingspeak.com/channels/${CHANNEL_ID}/fields/1.json`
+    `https://api.thingspeak.com/channels/${CHANNEL_ID}/fields/${fieldNumber}.json?results=500`
   );
   const data = await response.json();
 
-  window.myLine.data.datasets[0].data = [];
-  window.myLine.data.labels = [];
+  chartInstance.data.datasets[0].data = [];
+  chartInstance.data.labels = [];
 
   data.feeds.forEach(feed => {
-    window.myLine.data.datasets[0].data.push(feed.field1);
+    chartInstance.data.datasets[0].data.push(feed[`field${fieldNumber}`]);
 
     const time = new Date(feed.created_at);
     const hours = time.getHours().toString().padStart(2, '0');
     const minutes = time.getMinutes().toString().padStart(2, '0');
-    window.myLine.data.labels.push(`${hours}:${minutes}`);
+    chartInstance.data.labels.push(`${hours}:${minutes}`);
   });
 
-  window.myLine.update();
+  chartInstance.update();
 }
 
-getLastThingSpeakData();
+function getRawChartsData() {
+  getLastThingSpeakData({
+    chartInstance: window.rawField1,
+    fieldNumber: 1,
+  });
+  
+  getLastThingSpeakData({
+    chartInstance: window.rawField2,
+    fieldNumber: 2,
+  });
+  
+  getLastThingSpeakData({
+    chartInstance: window.rawField3,
+    fieldNumber: 3,
+  });
+  
+  getLastThingSpeakData({
+    chartInstance: window.rawField4,
+    fieldNumber: 4,
+  });
+}
+
+getRawChartsData();
 
 setInterval(() => {
-  getLastThingSpeakData();
-}, 1000 * 60);
+  getRawChartsData();
+}, 1000 * 60 * 5);
--- iot_dashboard/views.py
+++ iot_dashboard/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
+import requests
 
 def home(request):
    return render(request, 'index.html')
 
 def send_json(request):
-   data = [{'name': 'Peter', 'email': 'peter@example.org'},
-         {'name': 'Julia', 'email': 'julia@example.org'}]
+   payload = {'results': 500}
 
-   return JsonResponse(data, safe=False)
+   field_1 = requests.get('https://api.thingspeak.com/channels/196384/fields/1.json', params=payload)
+
+   return JsonResponse(field_1.json(), safe=False)
